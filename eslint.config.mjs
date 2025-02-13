25cae993d372a41e61ac55f9bd1e697ab6b18156
louis-stintzy <louis.stintzy@gmail.com>
2025-02-13 10:46:19 +0000

    chore: configure TypeScript, ESLint, and Prettier -2-

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,17 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    ignores: ['*.config.mjs'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
 );
